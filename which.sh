6f9b73d2e8201addbc5fd0c21a34a1b2fe39dae4
Frayzen <florian.laine@epita.fr>
2025-03-18 00:39:27 +0100

    feat: add comments

--- which.sh
+++ which.sh
@@ -1,59 +1,58 @@
- #!/bin/sh
+#!/bin/sh
 
+# Enable failure on error returned  
 set -e
 
-first=true
-allmatch=false
+allmatch=false # Option '-a' see man
 
-while getopts ":a" curopt
+while getopts ":a" curopt # Parses the arguments
 do
   case $curopt in
-    a) allmatch=true
+    a) allmatch=true # option '-a' present
        ;;
-    ?) 
+    ?) # Error on invalid argument
       echo "which: unknown option -- ${OPTARG}" 1>&2
       echo "usage: which [-a] name ..." 1>&2
       exit 1
       ;;
   esac
 done
-shift $(($OPTIND - 1))
+shift $(($OPTIND - 1)) # Get rid of parsed arguments 
 
-if [ $# -eq 0 ]; then
+if [ $# -eq 0 ]; then # Check for absence of argument
   echo "usage: which [-a] name ..." 1>&2
   exit 1
 fi
 
-all=true
-some=false
-for arg in "$@"; do
-  found=false
-  oldIFS="$IFS"
-  IFS=":" 
+all=true # Flag set when all arguments have a match
+some=false # Flag set when any argument has a match 
+for arg in "$@"; do # Loop on the arguments
+  found=false # Flag set if the current argument has a match
+  oldIFS="$IFS" # Save the IFS
+  IFS=":" # Split the PATH variable by semicolomn
   for path in $PATH; do
     file="$path/$arg"
-    if [ -x $file ]; then
+    if [ -x $file -a -f $file ]; then # If the path is a file and has execute access
       echo $file
-      found=true
-      if ! $allmatch; then
-        break
+      found=true # Set the flag
+      if ! $allmatch; then # If the '-a' option is not set
+        break # Stop after a single matching path 
       fi
     fi
   done
-  if ! $found; then
+  if ! $found; then # If current has not been found in $PATH
     echo "which: $arg: Command not found." 1>&2
     all=false
-  fi
-  if $found; then
+  else # Otherwise
     some=true
   fi
-  IFS="$oldIFS"
+  IFS="$oldIFS" # Restore IFS
 done
 
-if $all; then
+if $all; then # All files found
   exit 0
-elif $some; then
+elif $some; then # Some file found
   exit 1
-else #none
+else # No file found
   exit 2
 fi
